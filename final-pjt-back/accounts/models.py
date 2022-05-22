5eba03882f1ac25b0bf0255fbb6b5501945a7eca
sc-choi2022 <c.martin20222023@gmail.com>
2022-05-22 21:33:25 +0900

    models.py done

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from movies.models import Movie
 
-# Create your models here.
+class User(AbstractUser):
+    users_mymovie = models.ManyToManyField(Movie, related_name='movies_mymovie')
+    users_wish = models.ManyToManyField(Movie, related_name='movies_wish')
--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    orginal_title = models.CharField(max_length=100)
+    overview = models.TextField()
+    poster_path = models.CharField(max_length=200)
+    release_date = models.DateTimeField()
+    runtime = models.IntegerField()
+    video = models.CharField(max_length=100)
+    vote_average = models.FloatField()
+    vote_count = models.IntegerField()
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    movies = models.ManyToManyField(Movie, related_name='genres')
+
+class Certification(models.Model):
+    name = models.CharField(max_length=50)
+    movies = models.ManyToManyField(Movie, related_name='certifications')
+
+class Actor(models.Model):
+    name = models.CharField(max_length=50)
+    profile_path = models.CharField(max_length=200)
+    movies = models.ManyToManyField(Movie, related_name='actors')
+
+class Director(models.Model):
+    name = models.CharField(max_length=50)
+    movies = models.ManyToManyField(Movie, related_name='directors')
+
+class OTT(models.Model):
+    name = models.CharField(max_length=50)
+    movies =  models.ManyToManyField(Movie, related_name='otts')
+
+class Keyword(models.Model):
+    name = models.CharField(max_length=50)
+    movies = models.ManyToManyField(Movie, related_name='keywords')
