67007af1e6d322fe110734e6b0b53428f70cbb72
sc-choi2022 <c.martin20222023@gmail.com>
2022-05-23 19:41:52 +0900

    django_back_CORS

--- final-pjt-back/django_back/settings.py
+++ final-pjt-back/django_back/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'community',
     'movies',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +131,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+# 모두에게 교차출처 허용 (*)
+CORS_ALLOW_ALL_ORIGINS = True
