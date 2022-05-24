38d4ebb3a9ab4a70a28ad74ef98e787db64e9900
최수연 <martin0125@naver.com>
2022-05-24 20:59:26 +0900

    django_back_register_admin

--- final-pjt-back/movies/admin.py
+++ final-pjt-back/movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Movie, Genre, Certification, Actor, Director, OTT, Keyword
 
-# Register your models here.
+admin.site.register(Movie)
+admin.site.register(Genre)
+admin.site.register(Certification)
+admin.site.register(Actor)
+admin.site.register(Director)
+admin.site.register(OTT)
+admin.site.register(Keyword)
