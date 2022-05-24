874a7fbce5837af99a95ad6035d6d8d6e6a63abd
최수연 <martin0125@naver.com>
2022-05-24 20:56:57 +0900

    django_back_urls_views

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name ='movies'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('movie/<int:movie_pk>/', views.movie_detail, name='movie_detail'),
+    path('movie/<int:movie_id>/', views.movie_detail, name='movie_detail'),
     path('recommend/otts/<int:ott_id>/', views.recommend_otts, name='recommend_otts'),
     path('recommend/directors/<int:director_id>/', views.recommend_directors, name='recommend_directors'),
     path('recommend/keywords/<int:keyword_id>/', views.recommend_keywords, name='recommend_keywords'),
--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import get_object_or_404
 from movies.models import Movie
-import random
-
+from movies.serializers import MovieMainListSerializer, MovieSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import random
 
-from movies.serializers import MovieMainListSerializer, MovieSerializer
-
-# Create your views here.
 @api_view(['GET'])
 def index(request):
     keyword_list = ['2343', '9840', '180547', '18035', '12565','4344']
@@ -15,12 +12,10 @@ def index(request):
     movies = Movie.objects.filter(keywords=keyword).order_by('?')[:10]    
     serializer = MovieMainListSerializer(movies,many=True)  
     return Response(serializer.data)
-    # random으로 뽑힌 keyword id 값 넣어주는거 고민해보기
-
 
 @api_view(['GET'])
-def movie_detail(request, movie_pk):
-    movie = get_object_or_404(Movie, pk=movie_pk)
+def movie_detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
     serializer = MovieSerializer(movie)
     return Response(serializer.data)
 
